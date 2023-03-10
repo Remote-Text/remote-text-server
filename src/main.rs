8819a5f719ea3360093cd9bfcb833eb23fbabfd4
Vaughn <vzaayer@reed.edu>
2023-03-09 16:20:37 -0800

    Changed main.rs to have warp Hello World program, added tokio and warp to dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use warp::Filter;
+
+
+
+#[tokio::main]
+async fn main() {
+    let hello = warp::any().map(|| "Hello, World!");
+
+    warp::serve(hello)
+        .run(([127, 0, 0, 1], 3030))
+        .await;
 }
