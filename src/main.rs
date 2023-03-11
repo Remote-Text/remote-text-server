7492772326efa1b1fe122b818076a0c3b03618a7
Sam <30577766+Samasaur1@users.noreply.github.com>
2023-03-10 16:38:58 -0800

    Return JSON instead of raw text

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,17 @@
 use warp::Filter;
+use serde::Serialize;
 
 
+#[derive(Serialize, Copy, Clone)]
+struct Hello {
+    hello: &'static str
+}
 
 #[tokio::main]
 async fn main() {
-    let hello = warp::any().map(|| "Hello, World!");
+    let h = Hello { hello: "world" };
+    let hello = warp::any()
+        .map(move || warp::reply::json(&h))
 
     warp::serve(hello)
         .run(([127, 0, 0, 1], 3030))
