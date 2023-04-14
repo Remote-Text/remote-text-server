33091c7229cb1ccf9ada28a4aa049bc1a1b63f70
Sam <30577766+Samasaur1@users.noreply.github.com>
2023-04-13 23:42:50 -0700

    Output a more sane string for the dates in JSON

--- src/api.rs
+++ src/api.rs
@@ -2,6 +2,49 @@ use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 
+mod normal_date_format {
+    use chrono::{DateTime, Utc, TimeZone};
+    use serde::{self, Deserialize, Serializer, Deserializer};
+
+    const FORMAT: &'static str = "%Y-%m-%dT%H:%M:%SZ";
+    // 2023-04-14T06:29:29Z
+
+    // The signature of a serialize_with function must follow the pattern:
+    //
+    //    fn serialize<S>(&T, S) -> Result<S::Ok, S::Error>
+    //    where
+    //        S: Serializer
+    //
+    // although it may also be generic over the input types T.
+    pub fn serialize<S>(
+        date: &DateTime<Utc>,
+        serializer: S,
+    ) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let s = format!("{}", date.format(FORMAT));
+        serializer.serialize_str(&s)
+    }
+
+    // The signature of a deserialize_with function must follow the pattern:
+    //
+    //    fn deserialize<'de, D>(D) -> Result<T, D::Error>
+    //    where
+    //        D: Deserializer<'de>
+    //
+    // although it may also be generic over the output types T.
+    pub fn deserialize<'de, D>(
+        deserializer: D,
+    ) -> Result<DateTime<Utc>, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let s = String::deserialize(deserializer)?;
+        Utc.datetime_from_str(&s, FORMAT).map_err(serde::de::Error::custom)
+    }
+}
+
 #[derive(Serialize, Deserialize, Clone)]
 pub(crate) struct File {
     pub(crate) name: String,
@@ -13,7 +56,9 @@ pub(crate) struct File {
 pub(crate) struct FileSummary {
     pub(crate) name: String,
     pub(crate) id: Uuid,
+    #[serde(with = "normal_date_format")]
     pub(crate) edited_time: DateTime<Utc>,
+    #[serde(with = "normal_date_format")]
     pub(crate) created_time: DateTime<Utc>
 }
 
