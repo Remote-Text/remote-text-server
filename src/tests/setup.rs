a9f06befaa06b40513af39e69f81d5b12cabe385
Vaughn <vzaayer@reed.edu>
2023-04-21 09:15:05 -0700

    Fixed test_get_file test

--- src/tests/setup.rs
+++ src/tests/setup.rs
@@ -1,8 +1,12 @@
+#[cfg(test)]
+
 // #[macro_use] extern crate log;
 extern crate pretty_env_logger;
 
 use std::io::Bytes;
+use std::ops::Deref;
 use std::path::{Path, PathBuf};
+use std::process::id;
 use std::str::Utf8Error;
 
 use chrono::{DateTime, Utc};
@@ -18,6 +22,8 @@ use serde_json;
 
 use crate::{files, routes, api, handlers};
 use crate::files::repos;
+use crate::handlers::FileIDAndGitHash;
+use crate::routes::get_file;
 
 
 fn FILES_DIR() -> PathBuf {
@@ -28,10 +34,11 @@ fn PREVIEWS_DIR() -> PathBuf {
 }
 
 
-
-
 #[tokio::test]
 async fn test_list_files_filter() {
+
+    let _ = pretty_env_logger::try_init();
+
     let repositories = repos();
     let filter = routes::list_files(repositories);
 
@@ -47,6 +54,9 @@ async fn test_list_files_filter() {
 
 #[tokio::test]
 async fn test_create_file_filter() {
+
+    let _ = pretty_env_logger::try_init();
+
     let repositories = repos();
     let filter = routes::create_file(repositories);
 
@@ -67,6 +77,9 @@ async fn test_create_file_filter() {
 
 #[tokio::test]
 async fn create_file_too_large() {
+
+    let _ = pretty_env_logger::try_init();
+
     let repositories = repos();
     let filter = routes::create_file(repositories);
 
@@ -84,4 +97,59 @@ async fn create_file_too_large() {
         .await;
 
     assert_eq!(result.status(), 413);
+}
+
+#[tokio::test]
+async fn test_get_file() {
+
+    let _ = pretty_env_logger::try_init();
+
+    let repositories = repos();
+
+    // Finding a file requires both an ID and GitHash
+
+    // First we create a file
+    let obj = handlers::NameAndOptionalContent{ name: "TestFile".to_string(), content: None };
+    let filter = routes::create_file(repositories.clone());
+
+
+    let result = test::request()
+        .method("POST")
+        .path("/createFile")
+        .json(&obj)
+        .reply(&filter)
+        .await;
+
+    assert_eq!(result.status(), 200);
+
+    let deserializedResult : api::FileSummary = serde_json::from_slice(result.body()).unwrap();
+
+    assert_eq!(deserializedResult.name, "TestFile");
+
+    let getFileInfo = {
+        let fileGitHash = repositories
+            .lock()
+            .unwrap();
+
+        let fileGitHash = fileGitHash.deref().get(&deserializedResult.id).unwrap();
+
+        let rawGitHash = "HEAD";
+        let rawGitHash = fileGitHash.revparse_single(rawGitHash).unwrap();
+
+        FileIDAndGitHash { id: deserializedResult.id, hash: rawGitHash.id().to_string() }
+    };
+
+
+
+    let filter = get_file(repositories.clone());
+    let obj = getFileInfo;
+
+    let result = test::request()
+        .method("POST")
+        .path("/getFile")
+        .json(&obj)
+        .reply(&filter)
+        .await;
+
+    assert_eq!(result.status(), 200);
 }
