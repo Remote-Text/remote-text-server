bff88cf8ad3d907cc7c47d191fa19bc25a6429f6
Sam <30577766+Samasaur1@users.noreply.github.com>
2024-01-07 10:37:37 +0100

    Add creation date to Docker image

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "The server-side software for Remote Text";
 
-  outputs = { nixpkgs, ... }:
+  outputs = { self, nixpkgs, ... }:
     let
       forAllSystems = gen:
         nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed
@@ -12,6 +12,8 @@
         default = remote-text-server;
         dockerImage = pkgs.dockerTools.buildImage {
           name = "remote-text-server";
+          # Based on the last commit date, see: https://nixos.wiki/wiki/Docker#Reproducible_image_dates
+          created = builtins.substring 0 8 self.lastModifiedDate;
           config = {
             Cmd = [ "${remote-text-server}/bin/remote-text-server" ];
           };
