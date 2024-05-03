49b62fa250e0a168fc220df660b11a595c4b9720
Sam <30577766+Samasaur1@users.noreply.github.com>
2024-05-03 02:08:28 -0700

    Improve request body size limits
    This increases the request body size limit on saveFile and createFile,
    since they pass the entire content of the file, while lowering the
    request body size limit on all other requests. The "big" limit is now
    16MiB (up from 16KiB) and the "small" limit is now 4KiB (down from
    16KiB). I think the small one could probably be much smaller.
    
    Closes #25

--- src/routes.rs
+++ src/routes.rs
@@ -8,9 +8,15 @@ use warp::Filter;
 
 use crate::handlers;
 
-// Filter that limits the size of JSON files
-pub(crate) fn json_body<T: DeserializeOwned + Send>() -> impl Filter<Extract = (T,), Error = warp::Rejection> + Clone {
-    warp::body::content_length_limit(1024 * 16)
+// Filter that limits requests to 4KiB and deserializes JSON
+pub(crate) fn json_body_small<T: DeserializeOwned + Send>() -> impl Filter<Extract = (T,), Error = warp::Rejection> + Clone {
+    warp::body::content_length_limit(1024 * 4)
+        .and(warp::body::json())
+}
+
+// Filter that limits requests to 16MiB and deserializes JSON
+pub(crate) fn json_body_big<T: DeserializeOwned + Send>() -> impl Filter<Extract = (T,), Error = warp::Rejection> + Clone {
+    warp::body::content_length_limit(1024 * 1024 * 16)
         .and(warp::body::json())
 }
 
@@ -23,7 +29,7 @@ pub(crate) fn list_files(repos: Arc<Mutex<HashMap<Uuid, Repository>>>) -> impl F
 // Filter that maps to the create_file api call, uses the json_body to restrict file size, then tries to fulfill the request
 pub(crate) fn create_file(repos: Arc<Mutex<HashMap<Uuid, Repository>>>) -> impl Filter<Extract = (impl warp::Reply,), Error = warp::Rejection> + Clone {
     warp::path("createFile")
-        .and(json_body())
+        .and(json_body_big())
         .and(warp::filters::addr::remote())
         .and_then(move |obj, addr| handlers::create_file(obj, addr, repos.clone()))
 }
@@ -31,14 +37,14 @@ pub(crate) fn create_file(repos: Arc<Mutex<HashMap<Uuid, Repository>>>) -> impl 
 // Filter that maps to the get_file api call, uses the json_body to restrict file size, then tries to fulfill the request
 pub(crate) fn get_file(repos: Arc<Mutex<HashMap<Uuid, Repository>>>) -> impl Filter<Extract = (impl warp::Reply,), Error = warp::Rejection> + Clone {
     warp::path("getFile")
-        .and(json_body())
+        .and(json_body_small())
         .and_then(move |obj| handlers::get_file(obj, repos.clone()))
 }
 
 // Filter that maps to the save_file api call, uses the json_body to restrict file size, then tries to fulfill the request
 pub(crate) fn save_file(repos: Arc<Mutex<HashMap<Uuid, Repository>>>) -> impl Filter<Extract = (impl warp::Reply,), Error = warp::Rejection> + Clone {
     warp::path("saveFile")
-        .and(json_body())
+        .and(json_body_big())
         .and(warp::filters::addr::remote())
         .and_then(move |obj, addr| handlers::save_file(obj, addr, repos.clone()))
 }
@@ -46,28 +52,28 @@ pub(crate) fn save_file(repos: Arc<Mutex<HashMap<Uuid, Repository>>>) -> impl Fi
 // Filter that maps to the delete_file api call, then attempts to fufill the request using handler code
 pub(crate) fn delete_file(repos: Arc<Mutex<HashMap<Uuid, Repository>>>) -> impl Filter<Extract = (impl warp::Reply,), Error = warp::Rejection> + Clone {
     warp::path("deleteFile")
-        .and(json_body())
+        .and(json_body_small())
         .and_then(move |obj| handlers::delete_file(obj, repos.clone()))
 }
 
 // Filter that maps to the preview_file api call, uses the json_body to restrict file size, then tries to fulfill the request
 pub(crate) fn preview_file(repos: Arc<Mutex<HashMap<Uuid, Repository>>>) -> impl Filter<Extract = (impl warp::Reply,), Error = warp::Rejection> + Clone {
     warp::path("previewFile")
-        .and(json_body())
+        .and(json_body_small())
         .and_then(move |obj| handlers::preview_file(obj, repos.clone()))
 }
 
 // Filter that maps to the get_preview api call, uses the json_body to restrict file size, then tries to fulfill the request
 pub(crate) fn get_preview(repos: Arc<Mutex<HashMap<Uuid, Repository>>>) -> impl Filter<Extract = (impl warp::Reply,), Error = warp::Rejection> + Clone {
     warp::path("getPreview")
-        .and(json_body())
+        .and(json_body_small())
         .and_then(move |obj| handlers::get_preview(obj, repos.clone()))
 }
 
 // Filter that maps to the get_history api call, uses the json_body to restrict file size, then tries to fulfill the request
 pub(crate) fn get_history(repos: Arc<Mutex<HashMap<Uuid, Repository>>>) -> impl Filter<Extract = (impl warp::Reply,), Error = warp::Rejection> + Clone {
     warp::path("getHistory")
-        .and(json_body())
+        .and(json_body_small())
         .and_then(move |obj| handlers::get_history(obj, repos.clone()))
 }
 
